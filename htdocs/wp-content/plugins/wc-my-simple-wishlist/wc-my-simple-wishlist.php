64222ce1b374911b16c34475262baa17430d5ebb
Loïc HALL <lhall@amphibee.fr>
2025-03-14 12:49:53 +0100

    Ajout plugin wishlist

---
+++ htdocs/wp-content/plugins/wc-my-simple-wishlist/wc-my-simple-wishlist.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @package WPSimpleWishlist
+ * @version 1.0.0
+ */
+
+/*
+Plugin Name: WC Simple Wishlist
+Plugin URI: http://amphibee.fr
+Description: Add a product to your wishlist
+Author: Loïc Hall
+Version: 1.0.0
+*/
+
+require_once 'inc/Wishlist.php';
+require_once 'inc/Ajax.php';
+
+(new \SimpleWishlist\Wishlist())->register();
+(new \SimpleWishlist\Ajax())->register();
